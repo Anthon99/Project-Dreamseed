2b5b565b6f9c69788ed95e64a972a9b925bba848
BlakeGordon-RMIT <55981311+BlakeGordon-RMIT@users.noreply.github.com>
2019-10-14 22:12:35 +1100

    New version includes some changes
    New bits:
    -Draws a sky/land difference
    -Adds a 'magic resource' bar that interacts with the spell casting
    -Spell changing mode , press 1 and 2 to switch between spells.

--- project_dreamseed/dreamseed.py
+++ project_dreamseed/dreamseed.py
@@ -22,11 +22,44 @@ TILE_SCALING = 4
 MOB_SCALING = 0.5
 
 #spell casting globals
-SELECTED_SPELL = 1
+
 SPELL_CAST_X = 200
 SPELL_CAST_Y = 400
 
 FIRESPEED = 5
+WATERSPEED = 15
+
+#class to store variables for drawing the magic pool
+class Magic:
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.width = 0
+		self.height = 0
+		self.color = 0
+		self.s_angle = 0
+		self.f_angle = 0
+		self.tilt = 0
+		self.sections = 0
+	
+#Section for building functions
+
+#function to display the magic pool
+def make_magic():
+	magic = Magic()
+	magic.x = 100
+	magic.y = 700
+	magic.width = 40
+	magic.height = 40
+	magic.color = arcade.color.BLUE_VIOLET
+	magic.s_angle = 0
+	magic.f_angle = 160
+	magic.tilt = 0
+	magic.sections = 300
+	return magic
+
+
+
 
 class MyGame(arcade.Window):
 	"""
@@ -48,11 +81,29 @@ class MyGame(arcade.Window):
 		self.base_list = None
 		self.gui_list = None
 		self.cursor_sprite = None
+		self.magic_list = []
+		
+		magic = make_magic()
+		self.magic_list.append(magic)
+		
+		
 		self.tree = None
 		
+		#load spell casting variables
+		self.selected_spell = 1
+		self.magic_resource_percentage = 0.5
+		self.magic_resource_replenish = 1
+		self.magic_resource_spend_modifer = 1
+		
+		#load spell cost variables
+		self.firefury_cost = 0.07
+		self.waterblast_cost = 0.7
+		"""
+		This part is to set the variable settings for what is open 
+		"""
 		
 		self.set_mouse_visible(False)
-		arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+		
 
 	def setup(self):
 		# Create your sprites and sprite lists here
@@ -60,6 +111,7 @@ class MyGame(arcade.Window):
 		self.enem_list = arcade.SpriteList()
 		self.base_list = arcade.SpriteList()
 		self.gui_list = arcade.SpriteList()
+		
 		
 		
 		self.cursor_sprite = arcade.Sprite("images/cursor/cursor1.png", CHARACTER_SCALING)
@@ -80,13 +132,22 @@ class MyGame(arcade.Window):
 		# This command should happen before we start drawing. It will clear
 		# the screen to the background color, and erase what we drew last frame.
 		arcade.start_render()
-	
-
+		# Draw the sky in the top two-thirds
+		arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT * (1 / 3), arcade.color.SKY_BLUE)
+		# Draw the ground in the bottom third
+		arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.DARK_SPRING_GREEN)
+		
+		#Draw magic bar
+		for magic in self.magic_list:
+			arcade.draw_circle_filled(magic.x, magic.y, magic.height , arcade.color.LAVENDER_BLUE, 128)
+			arcade.draw_arc_filled(magic.x, magic.y, magic.width,  magic.height, magic.color, magic.s_angle, (360* self.magic_resource_percentage), magic.tilt, magic.sections)
+			arcade.draw_circle_outline(magic.x, magic.y, magic.height , arcade.color.REGALIA, 5, 128)
 		# Call draw() on all your sprite lists below
 		self.gui_list.draw()
 		self.spell_list.draw()
 		self.base_list.draw()
 		self.enem_list.draw()
+
 	
 
 	def update(self, delta_time):
@@ -95,21 +156,39 @@ class MyGame(arcade.Window):
 		Normally, you'll call update() on the sprite lists that
 		need it.
 		"""
-		self.spell_list.update()
 		
+		#update sprite lists
+		self.spell_list.update()
+		self.gui_list.update()
+		
+		#logic for spells
 		for firefury in self.spell_list:
 			if firefury.bottom > self.width or firefury.top < 0 or firefury.right < 0 or firefury.left > self.width:
 				firefury.kill()
 				
+		for waterblast in self.spell_list:
+			if waterblast.bottom > self.width or waterblast.top < 0 or waterblast.right < 0 or waterblast.left > self.width:
+				waterblast.kill()
+		#update magic resource bar
+		
+		
+		if self.magic_resource_percentage < 1:
+			self.magic_resource_percentage = (self.magic_resource_percentage + (self.magic_resource_replenish/60))
+			
+			
+
 
 	def on_key_press(self, key, key_modifiers):
-		"""
-		Called whenever a key on the keyboard is pressed.
-
-		For a full list of keys, see:
-		http://arcade.academy/arcade.key.html
-		"""
-		pass
+		
+		if key == (arcade.key.KEY_1 or arcade.key.NUM_1):
+			self.selected_spell = 1
+			print ("Spell 1 selected")
+		
+		if key == (arcade.key.KEY_2 or arcade.key.NUM_2):
+			self.selected_spell = 2
+			print ("Spell 2 selected")
+		
+		
 
 	def on_key_release(self, key, key_modifiers):
 		"""
@@ -131,10 +210,11 @@ class MyGame(arcade.Window):
 		Called when the user presses a mouse button.
 		"""
 		#check selected spell and draw spell at caster location
-		if SELECTED_SPELL == 1:
+		if (self.selected_spell == 1) and (self.magic_resource_percentage > (self.magic_resource_spend_modifer * self.firefury_cost)):
 			firefury = arcade.Sprite("images/spells/spell_firefury1.png", CHARACTER_SCALING)
 			firefury.center_x = SPELL_CAST_X
 			firefury.center_y = SPELL_CAST_Y
+			self.magic_resource_percentage = self.magic_resource_percentage - self.firefury_cost
 			
 			
 			#grab mouse position to calculate aim
@@ -156,6 +236,30 @@ class MyGame(arcade.Window):
 			#append to sprite list
 			self.spell_list.append(firefury)
 
+		elif (self.selected_spell == 2) and (self.magic_resource_percentage > (self.magic_resource_spend_modifer * self.waterblast_cost)):
+			waterblast = arcade.Sprite("images/spells/spell_waterblast1.png", CHARACTER_SCALING)
+			waterblast.center_x = SPELL_CAST_X
+			waterblast.center_y = SPELL_CAST_Y
+			self.magic_resource_percentage = self.magic_resource_percentage - self.waterblast_cost
+			
+			#grab mouse position to calculate aim
+			destination_x = x
+			destination_y = y
+			
+			difference_in_x = destination_x - SPELL_CAST_X
+			difference_in_y = destination_y - SPELL_CAST_Y
+			angle = math.atan2(difference_in_y, difference_in_x)
+			
+			#use calculated angle to rotate spell sprite
+			waterblast.angle = math.degrees(angle)
+			
+			#calc rates of change to move spell in right direction
+			
+			waterblast.change_x = math.cos(angle) * WATERSPEED
+			waterblast.change_y = math.sin(angle) * WATERSPEED
+			
+			#append to sprite list
+			self.spell_list.append(waterblast)
 	def on_mouse_release(self, x, y, button, key_modifiers):
 		"""
 		Called when a user releases a mouse button.
